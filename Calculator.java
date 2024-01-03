1f8c0895312afb5302d9f48852bbc693de7d2d52
kochhdh <133973952+kochhdh@users.noreply.github.com>
2024-01-03 20:28:17 +0300

    Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,109 +1,80 @@
-import com.sun.tools.javac.Main;
-
 import java.util.Scanner;
 
 public class Calculator {
-     static int convert_to_int(String romes_value){
-        char[] value_char = romes_value.toCharArray();
-        int[] values_int = new int[value_char.length];
-        for (int i = 0; i < value_char.length; i++) {
-            switch (value_char[i]) {
-                case 'I':
-                    values_int[i] = 1;
-                    break;
-                case 'V':
-                    values_int[i] = 5;
-                    break;
-                case 'X':
-                    values_int[i] = 10;
-                    break;
-                default:
-                    throw new ArithmeticException("throws Exception //т.к. формат математической операции не удовлетворяет заданию (+, -, /, *");
-            }
-        }
-        int result = values_int[0];
-        for (int i = 0; i < values_int.length && values_int.length > i + 1; i++) {
-            if (values_int[i] >= values_int[i+1]) {
-                result += values_int[i+1];
-            } else if (values_int[i] < values_int[i+1]) {
-                result = result + values_int[i+1] - 2;
-            }
+    public static String result = "";
+    public static char oper;
+    public static String[] string;
+    public static int number, number1;
 
-        }
-        return result;
-    }
-     static String integerToRoman(int num) {
-        int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        String[] romanLiterals = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-        StringBuilder roman = new StringBuilder();
-        for(int i=0;i<values.length;i++) {
-            while(num >= values[i]) {
-                num -= values[i];
-                roman.append(romanLiterals[i]);
-            }
-        }
-        return roman.toString();
-    }
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int resop, numb1, numb2;
-        char oper;
-        boolean arabicNumbers;
-        while (true) {
-            System.out.println("Input: ");
-            arabicNumbers = true;
-            Scanner inputString_a_value = new Scanner(System.in);
-            String inputString = inputString_a_value.nextLine();
-            String[] calc_inputString = inputString.split(" ");
-            if (calc_inputString.length == 5) {
-                System.out.println("throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
-                throw new ArithmeticException("throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
-            }
-            if (calc_inputString.length != 3) {
-                throw new ArithmeticException("throws Exception //т.к. строка не является математической операцией");
-            }
-            System.out.println("Output: ");
-            oper=calc_inputString[1].charAt(0);
-            numb1 = 0;
-            numb2 = 0;
-            resop = 0;
-            try {
-                numb1 = Integer.parseInt(calc_inputString[0]);
-                numb2 = Integer.parseInt(calc_inputString[2]);
-            } catch (NumberFormatException e) {
-                arabicNumbers = false;
-                numb1=convert_to_int(calc_inputString[0]);
-                numb2=convert_to_int(calc_inputString[2]);
 
-            }
-            if (((numb1>10) || (numb1<1)) || ((numb2>10) || (numb2<1))) {
-                throw new ArithmeticException("throws Exception //т.к. число меньше 1 и более 10");
-            }
-            switch (oper) {
-                case '+':
-                    resop = numb1 + numb2;
-                    break;
-                case '-':
-                    resop = numb1 - numb2;
-                    break;
-                case '*':
-                    resop = numb1 * numb2;
-                    break;
-                case '/':
-                    resop = numb1 / numb2;
-                    break;
-                default:
-                    throw new ArithmeticException("throws Exception //т.к. формат математической операции не удовлетворяет заданию (+, -, /, *");
-            }
 
-            if (arabicNumbers) {
-                System.out.println(resop);
+    public static  void main(String[] args) throws Exception{
+        Scanner scr = new Scanner(System.in);
+        String str = scr.nextLine();
+
+        if (str.contains(" + ")){
+            string = str.split(" \\+ ");
+            if (string[0].contains("\"") || string[1].contains("\"")) {
+                string[0] = string[0].replace("\"", "");
+                string[1] = string[1].replace("\"", "");
+            }else throw new Exception("Строка должна содержать кавычки");
+            if (string[0].length() > 10 || string[1].length() > 10) throw new Exception("длинна строки не может превышать 10 символов");
+            oper = '+';
+        } else if (str.contains(" - ")){
+            string = str.split(" - ");
+            if (string[0].contains("\"") || string[1].contains("\"")) {
+                string[0] = string[0].replace("\"", "");
+                string[1] = string[1].replace("\"", "");
+            }else throw new Exception("Строка должна содержать кавычки");
+            if (string[0].length() > 10 || string[1].length() > 10) throw new Exception("длинна строки не может превышать 10 символов");
+            oper = '-';
+        } else if (str.contains(" * ")){
+            string = str.split(" \\* ");
+            if (string[0].contains("\""))
+                string[0] = string[0].replace("\"", "");
+            else throw new Exception("Строка должна содержать кавычки");
+            if (string[0].length() > 10) throw new Exception("длинна строки не может превышать 10 символов");
+            if (string[1].contains("\"")) throw new Exception("число не должно быть с кавычками");
+            number = Integer.parseInt(string[1]);
+            if (number < 1 || number > 10) throw new Exception("число должно быть от 1 до 10");
+            oper = '*';
+        }else if (str.contains(" / ")) {
+            string = str.split(" / ");
+            if (string[0].contains("\""))
+                string[0] = string[0].replace("\"", "");
+            else throw new Exception("Строка должна содержать кавычки");
+            if (string[0].length() > 10) throw new Exception("длинна строки не может превышать 10 символов");
+            if (!string[1].matches("\\d")) throw new Exception("делить можно только на число");
+            number = Integer.parseInt(string[1]);
+            if (number < 1 || number > 10) throw new Exception("число должно быть от 1 до 10");
+            oper = '/';
+        } else
+            throw new Exception("введен не корректный знак");
+
+        if (oper == '+'){
+            result = string[0] + string[1];
+        }
+        if (oper == '-') {
+            number = string[0].indexOf(string[1]);
+            if (number == -1) {
+                result = string[0];
             } else {
-                if (resop < 0) {
-                    throw new ArithmeticException("throws Exception //т.к. в римской системе нет отрицательных чисел");
-                }
-                System.out.println(integerToRoman(resop));
+                result = string[0].substring(0, number);
+                result += string[0].substring(number + string[1].length());
             }
         }
+        if (oper == '*'){
+            for (int i = 0; i < number; i++) {
+                result += string[0];
+            }
+        }
+        if (oper == '/'){
+            number1 = string[0].length() / number;
+            result = string[0].substring(0,number1);
+        }
+        print(result.substring(0, 40) + "(...)");
+    }
+    public static void print(String text){
+        System.out.println("\"" + text + "\"");
     }
 }
